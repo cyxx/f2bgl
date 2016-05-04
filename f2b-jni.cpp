69403e39c7fe25b078f9b6ce8d6da9992ea28b7e
Gregory Montoir <cyx@users.sourceforge.net>
2016-05-04 08:38:54 +0800

    move keycode mapping to android jni

--- f2b-jni.cpp
+++ f2b-jni.cpp
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <sys/param.h>
 #include <jni.h>
+#include <android/keycodes.h>
 #include <android/log.h>
 #include <dlfcn.h>
 #include "stub.h"
@@ -52,7 +53,76 @@ JNIEXPORT void JNICALL Java_org_cyxdown_f2b_F2bJni_quitGame(JNIEnv *env, jclass 
 JNIEXPORT void JNICALL Java_org_cyxdown_f2b_F2bJni_queueKeyEvent(JNIEnv *env, jclass c, jint keycode, jint pressed) {
 	if (g_stub) {
 		__android_log_print(ANDROID_LOG_INFO, LOG_TAG, "keyEvent %d pressed %d", keycode, pressed);
-		g_stub->queueKeyInput(keycode, pressed);
+		int key = -1;
+		switch (keycode) {
+		case AKEYCODE_SPACE:
+			key = kKeyCodeSpace;
+			break;
+		case AKEYCODE_ALT_LEFT:
+		case AKEYCODE_ALT_RIGHT:
+			key = kKeyCodeAlt;
+			break;
+		case AKEYCODE_SHIFT_LEFT:
+		case AKEYCODE_SHIFT_RIGHT:
+			key = kKeyCodeShift;
+			break;
+		case AKEYCODE_SOFT_LEFT:
+		case AKEYCODE_SOFT_RIGHT:
+			key = kKeyCodeCtrl;
+			break;
+		case AKEYCODE_TAB:
+			key = kKeyCodeTab;
+			break;
+		case AKEYCODE_ENTER:
+		case AKEYCODE_DPAD_CENTER:
+			key = kKeyCodeReturn;
+			break;
+		case AKEYCODE_DPAD_UP:
+			key = kKeyCodeUp;
+			break;
+		case AKEYCODE_DPAD_DOWN:
+			key = kKeyCodeDown;
+			break;
+		case AKEYCODE_DPAD_LEFT:
+			key = kKeyCodeLeft;
+			break;
+		case AKEYCODE_DPAD_RIGHT:
+			key = kKeyCodeRight;
+			break;
+		case AKEYCODE_H:
+			key = kKeyCodeH;
+			break;
+		case AKEYCODE_I:
+			key = kKeyCodeI;
+			break;
+		case AKEYCODE_J:
+			key = kKeyCodeJ;
+			break;
+		case AKEYCODE_K:
+			key = kKeyCodeK;
+			break;
+		case AKEYCODE_U:
+			key = kKeyCodeU;
+			break;
+		case AKEYCODE_1:
+			key = kKeyCode1;
+			break;
+		case AKEYCODE_2:
+			key = kKeyCode2;
+			break;
+		case AKEYCODE_3:
+			key = kKeyCode3;
+			break;
+		case AKEYCODE_4:
+			key = kKeyCode4;
+			break;
+		case AKEYCODE_5:
+			key = kKeyCode5;
+			break;
+		}
+		if (key != -1) {
+			g_stub->queueKeyInput(key, pressed);
+		}
 	}
 }
 
