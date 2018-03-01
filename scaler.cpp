e179a1ea55d930204d1879e904e7e0bae38bfc9d
Gregory Montoir <cyx@users.sourceforge.net>
2018-03-01 20:12:38 +0800

    rewrite scale2x and scale3x routine to avoid border checking

--- scaler.cpp
+++ scaler.cpp
@@ -48,81 +48,245 @@ void point3x(uint16_t *dst, int dstPitch, const uint16_t *src, int srcPitch, int
 	}
 }
 
-void scale2x(uint16_t *dst, int dstPitch, const uint16_t *src, int srcPitch, int w, int h) {
-	for (int y = 0; y < h; ++y) {
-		uint16_t *p = dst;
-		for (int x = 0; x < w; ++x, p += 2) {
-			const uint16_t E = *(src + x);
-			uint16_t B = (y == 0) ? E : *(src + x - srcPitch);
-			uint16_t D = (x == 0) ? E : *(src + x - 1);
-			uint16_t F = (x == w - 1) ? E : *(src + x + 1);
-			uint16_t H = (y == h - 1) ? E : *(src + x + srcPitch);
-			if (B != H && D != F) {
-				*(p) = D == B ? D : E;
-				*(p + 1) = B == F ? F : E;
-				*(p + dstPitch) = D == H ? D : E;
-				*(p + dstPitch + 1) = H == F ? F : E;
-			} else {
-				*(p) = E;
-				*(p + 1) = E;
-				*(p + dstPitch) = E;
-				*(p + dstPitch + 1) = E;
-			}
+static void scanline2x(uint16_t *dst0, uint16_t *dst1, const uint16_t *src0, const uint16_t *src1, const uint16_t *src2, int w) {
+	uint16_t B, D, E, F, H;
+
+	// ABC
+	// DEF
+	// GHI
+
+	int x = 0;
+
+	// first pixel (D == E)
+	B = *(src0 + x);
+	E = *(src1 + x);
+	D = E;
+	F = *(src1 + x + 1);
+	H = *(src2 + x);
+	if (B != H && D != F) {
+		dst0[0] = D == B ? D : E;
+		dst0[1] = B == F ? F : E;
+		dst1[0] = D == H ? D : E;
+		dst1[1] = H == F ? F : E;
+	} else {
+		dst0[0] = E;
+		dst0[1] = E;
+		dst1[0] = E;
+		dst1[1] = E;
+	}
+	dst0 += 2;
+	dst1 += 2;
+
+	// center pixels
+	E = F;
+	for (x = 1; x < w - 1; ++x) {
+		B = *(src0 + x);
+		F = *(src1 + x + 1);
+		H = *(src2 + x);
+		if (B != H && D != F) {
+			dst0[0] = D == B ? D : E;
+			dst0[1] = B == F ? F : E;
+			dst1[0] = D == H ? D : E;
+			dst1[1] = H == F ? F : E;
+		} else {
+			dst0[0] = E;
+			dst0[1] = E;
+			dst1[0] = E;
+			dst1[1] = E;
 		}
-		dst += dstPitch * 2;
-		src += srcPitch;
+		D = E; E = F;
+		dst0 += 2;
+		dst1 += 2;
+	}
+
+	// last pixel (F == E)
+	B = *(src0 + x);
+	H = *(src2 + x);
+	if (B != H && D != F) {
+		dst0[0] = D == B ? D : E;
+		dst0[1] = B == F ? F : E;
+		dst1[0] = D == H ? D : E;
+		dst1[1] = H == F ? F : E;
+	} else {
+		dst0[0] = E;
+		dst0[1] = E;
+		dst1[0] = E;
+		dst1[1] = E;
+	}
+}
+
+void scale2x(uint16_t *dst, int dstPitch, const uint16_t *src, int srcPitch, int w, int h) {
+	if (w == 1 || h == 1) {
+		point2x(dst, dstPitch, src, srcPitch, w, h);
+		return;
+	}
+	assert(w > 1 && h > 1);
+	const int dstPitch2 = dstPitch * 2;
+
+	const uint16_t *src0, *src1, *src2;
+
+	// y == 0
+	src0 = src;
+	src1 = src;
+	src2 = src + srcPitch;
+	scanline2x(dst, dst + dstPitch, src0, src1, src2, w);
+	dst += dstPitch2;
+
+	// center
+	src0 = src;
+	src1 = src + srcPitch;
+	src2 = src + srcPitch * 2;
+	for (int y = 1; y < h - 1; ++y) {
+		scanline2x(dst, dst + dstPitch, src0, src1, src2, w);
+		dst += dstPitch2;
+
+		src0 += srcPitch;
+		src1 += srcPitch;
+		src2 += srcPitch;
+	}
+
+	// y == h-1
+	src2 = src1;
+	scanline2x(dst, dst + dstPitch, src0, src1, src2, w);
+}
+
+static void scanline3x(uint16_t *dst0, uint16_t *dst1, uint16_t *dst2, const uint16_t *src0, const uint16_t *src1, const uint16_t *src2, int w) {
+	uint16_t A, B, C, D, E, F, G, H, I;
+
+	// ABC
+	// DEF
+	// GHI
+
+	int x = 0;
+
+	// first pixel (A == B, D == E and G == H)
+	B = *(src0 + x);
+	A = B;
+	C = *(src0 + x + 1);
+	E = *(src1 + x);
+	D = E;
+	F = *(src1 + x + 1);
+	H = *(src2 + x);
+	G = H;
+	I = *(src2 + x + 1);
+	if (B != H && D != F) {
+		dst0[0] = D == B ? D : E;
+		dst0[1] = (E == B && E != C) || (B == F && E != A) ? B : E;
+		dst0[2] = B == F ? F : E;
+		dst1[0] = (D == B && E != G) || (D == B && E != A) ? D : E;
+		dst1[1] = E;
+		dst1[2] = (B == F && E != I) || (H == F && E != C) ? F : E;
+		dst2[0] = D == H ? D : E;
+		dst2[1] = (D == H && E != I) || (H == F && E != G) ? H : E;
+		dst2[2] = H == F ? F : E;
+	} else {
+		dst0[0] = E;
+		dst0[1] = E;
+		dst0[2] = E;
+		dst1[0] = E;
+		dst1[1] = E;
+		dst1[2] = E;
+		dst2[0] = E;
+		dst2[1] = E;
+		dst2[2] = E;
+	}
+	dst0 += 3;
+	dst1 += 3;
+	dst2 += 3;
+
+	// center pixels
+	B = C;
+	E = F;
+	H = I;
+	for (x = 1; x < w - 1; ++x) {
+		C = *(src0 + x + 1);
+		F = *(src1 + x + 1);
+		I = *(src2 + x + 1);
+		if (B != H && D != F) {
+			dst0[0] = D == B ? D : E;
+			dst0[1] = (E == B && E != C) || (B == F && E != A) ? B : E;
+			dst0[2] = B == F ? F : E;
+			dst1[0] = (D == B && E != G) || (D == B && E != A) ? D : E;
+			dst1[1] = E;
+			dst1[2] = (B == F && E != I) || (H == F && E != C) ? F : E;
+			dst2[0] = D == H ? D : E;
+			dst2[1] = (D == H && E != I) || (H == F && E != G) ? H : E;
+			dst2[2] = H == F ? F : E;
+		} else {
+			dst0[0] = E;
+			dst0[1] = E;
+			dst0[2] = E;
+			dst1[0] = E;
+			dst1[1] = E;
+			dst1[2] = E;
+			dst2[0] = E;
+			dst2[1] = E;
+			dst2[2] = E;
+		}
+		A = B; B = C;
+		D = E; E = F;
+		G = H; H = I;
+		dst0 += 3;
+		dst1 += 3;
+		dst2 += 3;
+	}
+
+	// last pixel (B == C, E == F and H == I)
+	if (B != H && D != F) {
+		dst0[0] = D == B ? D : E;
+		dst0[1] = (E == B && E != C) || (B == F && E != A) ? B : E;
+		dst0[2] = B == F ? F : E;
+		dst1[0] = (D == B && E != G) || (D == B && E != A) ? D : E;
+		dst1[1] = E;
+		dst1[2] = (B == F && E != I) || (H == F && E != C) ? F : E;
+		dst2[0] = D == H ? D : E;
+		dst2[1] = (D == H && E != I) || (H == F && E != G) ? H : E;
+		dst2[2] = H == F ? F : E;
+	} else {
+		dst0[0] = E;
+		dst0[1] = E;
+		dst0[2] = E;
+		dst1[0] = E;
+		dst1[1] = E;
+		dst1[2] = E;
+		dst2[0] = E;
+		dst2[1] = E;
+		dst2[2] = E;
 	}
 }
 
 void scale3x(uint16_t *dst, int dstPitch, const uint16_t *src, int srcPitch, int w, int h) {
-	for (int y = 0; y < h; ++y) {
-		uint16_t *p = dst;
-		for (int x = 0; x < w; ++x, p += 3) {
-			const uint16_t E = *(src + x);
-			uint16_t B = (y == 0) ? E : *(src + x - srcPitch);
-			uint16_t D = (x == 0) ? E : *(src + x - 1);
-			uint16_t F = (x == w - 1) ? E : *(src + x + 1);
-			uint16_t H = (y == h - 1) ? E : *(src + x + srcPitch);
-			uint16_t A, C;
-			if (y == 0) {
-				A = D;
-				C = F;
-			} else {
-				A = (x == 0) ? B : *(src + x - srcPitch - 1);
-				C = (x == w - 1) ? B : *(src + x - srcPitch + 1);
-			}
-			uint16_t G, I;
-			if (y == h - 1) {
-				G = D;
-				I = F;
-			} else {
-				G = (x == 0) ? H : *(src + x + srcPitch - 1);
-				I = (x == w - 1) ? H : *(src + x + srcPitch + 1);
-			}
-			if (B != H && D != F) {
-				*(p) = D == B ? D : E;
-				*(p + 1) = (D == B && E != C) || (B == F && E != A) ? B : E;
-				*(p + 2) = B == F ? F : E;
-				*(p + dstPitch) = (D == B && E != G) || (D == B && E != A) ? D : E;
-				*(p + dstPitch + 1) = E;
-				*(p + dstPitch + 2) = (B == F && E != I) || (H == F && E != C) ? F : E;
-				*(p + 2 * dstPitch) = D == H ? D : E;
-				*(p + 2 * dstPitch + 1) = (D == H && E != I) || (H == F && E != G) ? H : E;
-				*(p + 2 * dstPitch + 2) = H == F ? F : E;
-			} else {
-				*(p) = E;
-				*(p + 1) = E;
-				*(p + 2) = E;
-				*(p + dstPitch) = E;
-				*(p + dstPitch + 1) = E;
-				*(p + dstPitch + 2) = E;
-				*(p + 2 * dstPitch) = E;
-				*(p + 2 * dstPitch + 1) = E;
-				*(p + 2 * dstPitch + 2) = E;
-			}
-		}
-		dst += dstPitch * 3;
-		src += srcPitch;
+	if (w == 1 || h == 1) {
+		point3x(dst, dstPitch, src, srcPitch, w, h);
+		return;
 	}
-}
+	assert(w > 1 && h > 1);
+	const int dstPitch2 = dstPitch * 2;
+	const int dstPitch3 = dstPitch * 3;
 
+	const uint16_t *src0, *src1, *src2;
+
+	// y == 0
+	src0 = src;
+	src1 = src;
+	src2 = src + srcPitch;
+	scanline3x(dst, dst + dstPitch, dst + dstPitch2, src0, src1, src2, w);
+	dst += dstPitch3;
+
+	// center
+	src0 = src;
+	src1 = src + srcPitch;
+	src2 = src + srcPitch * 2;
+	for (int y = 1; y < h - 1; ++y) {
+		scanline3x(dst, dst + dstPitch, dst + dstPitch2, src0, src1, src2, w);
+		dst += dstPitch3;
+
+		src0 += srcPitch;
+		src1 += srcPitch;
+		src2 += srcPitch;
+	}
+
+	// y == h-1
+	src2 = src1;
+	scanline3x(dst, dst + dstPitch, dst + dstPitch2, src0, src1, src2, w);
+}
