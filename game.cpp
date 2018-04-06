7bef95f2f7c29a083015041ee8fe5fc6125155e1
Gregory Montoir <cyx@users.sourceforge.net>
2018-04-06 15:04:33 +0800

    disable shoot button to step control tweak (kCheatShootButtonToStep)
    'ctrl' is used to throw mines.

--- game.cpp
+++ game.cpp
@@ -14,7 +14,7 @@
 Game::Game(Render *render, const GameParams *params)
 	: _cut(render, this, &_snd), _snd(&_res), _render(render), _params(*params) {
 
-	_cheats = kCheatAutoReloadGun | kCheatActivateButtonToShoot | kCheatStepWithUpDownInShooting | kCheatShootButtonToStep;
+	_cheats = kCheatAutoReloadGun | kCheatActivateButtonToShoot | kCheatStepWithUpDownInShooting;
 	_gameStateMsg = 0;
 
 	memset(&_drawCharBuf, 0, sizeof(_drawCharBuf));
